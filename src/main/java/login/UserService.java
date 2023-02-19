749ad964077874e5682c747bfde2f4a42f33226d
raman suliman <75031065+RamanSuliman@users.noreply.github.com>
2023-02-19 21:48:37 +0000

    Appending fetch user by ID and checking the session status.

--- src/main/java/login/UserService.java
+++ src/main/java/login/UserService.java
@@ -1,7 +1,11 @@
 package login;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import repositories.UserRepository;
 
@@ -16,9 +20,9 @@ public class UserService
 		userRepository.save(user);
 	}
 	
-	public User login(String username, String password)
+	public User login(String email, String password)
 	{
-		User user = userRepository.findByUsername(username);
+		User user = userRepository.findByEmail(email);
 		if(user != null && user.getPassword().equals(password))
 			return user;
 		return null;
@@ -29,9 +33,28 @@ public class UserService
 		return userRepository.findByEmail(email);
 	}
 	
+	public User findByID(long id)
+	{
+		return userRepository.findById(id);
+	}
+	
 	public void resetPassword(User user, String newPassword)
 	{
 		user.setPassword(newPassword);
 		userRepository.save(user);
+	}
+	
+	public boolean isUserLogged()
+	{
+		// Get the current request and session
+		ServletRequestAttributes attr = (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
+		HttpSession session = attr.getRequest().getSession(true);
+
+		// Check if the user is logged in
+		if(session.getAttribute("userID") == null)
+			// If the user is not logged in
+	        return false;
+	    // If the user is logged in, display their profile
+	    return true;
 	}
 }
