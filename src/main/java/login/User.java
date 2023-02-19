e70b3619dab55fcd8656e1c034b8d818d56ab05d
raman suliman <75031065+RamanSuliman@users.noreply.github.com>
2023-02-19 21:51:52 +0000

    Javax persistence annotations marked User object to match users table.

---
+++ src/main/java/login/User.java
@@ -0,0 +1,40 @@
+package login;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User 
+{
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+	@Column(nullable = false)
+	private String password;
+	@Column(nullable = false, unique = true)
+	private String email;
+	
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+}
